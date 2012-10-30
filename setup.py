843d9258ef472813b0e1860648fced01fb9b2e78
Corry Haines <tabletcorry@gmail.com>
2012-10-30 13:27:56 -0700

    Make this a proper python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dt_utils',
+      version='1.0',
+      description='Provides an interface for DJB daemontools',
+      author='Corry Haines',
+      author_email='tabletcorry@gmail.com',
+      url='https://github.com/tabletcorry/dt_utils',
+      packages=[
+          'dt_utils',
+          ]
+     )
